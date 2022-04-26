181bbdcd8049329c044935db7741c43a2398ca9d
JasAndSmith <huchknows@gmail.com>
2022-04-25 21:16:35 -0400

    feat: routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RecipeEntryComponent } from './components/recipe-entry/recipe-entry.component';
+import { RecipeBrowserComponent } from './components/recipe-browser/recipe-browser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    RecipeEntryComponent,
+    RecipeBrowserComponent
   ],
   imports: [
     BrowserModule,
